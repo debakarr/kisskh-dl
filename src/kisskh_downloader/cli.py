a02aa05ecd77325e48a995c7be40f538b3b042c5
Debakar Roy <allinonedibakar@gmail.com>
2022-07-30 22:10:32 +0530

    - enable yt-dlp - remove tqdm, m3u8 and ffmpeg-python - enable download of subtitles

--- src/kisskh_downloader/cli.py
+++ src/kisskh_downloader/cli.py
@@ -1,16 +1,13 @@
-import os
-import shutil
 import sys
 from pathlib import Path
 from urllib.parse import parse_qs, urlparse
 
 import click
-import ffmpeg
 import validators
 
-from kisskh_downloader.downloader import Mu3u8Downloader
+from kisskh_downloader.downloader import Downloader
+from kisskh_downloader.enums.quality import Quality
 from kisskh_downloader.kisskh_api import KissKHApi
-from kisskh_downloader.utils import get_matching_quality
 
 
 @click.group()
@@ -20,18 +17,21 @@ def kisskh():
 
 @kisskh.command()
 @click.argument("drama_url_or_name")
-@click.option(
-    "--first", "-f", type=click.INT, default=1, help="Starting episode number."
-)
-@click.option(
-    "--last", "-l", type=click.INT, default=sys.maxsize, help="Ending episode number."
-)
+@click.option("--first", "-f", type=click.INT, default=1, help="Starting episode number.")
+@click.option("--last", "-l", type=click.INT, default=sys.maxsize, help="Ending episode number.")
 @click.option(
     "--quality",
     "-q",
     default="1080p",
-    type=click.Choice(["360p", "480p", "540p", "720p", "1080p"]),
+    type=click.Choice([quality.value for quality in Quality]),
     help="Quality of the video to be downloaded.",
+)
+@click.option(
+    "--sub-langs",
+    "-s",
+    default=("en",),
+    multiple=True,
+    help="Languages of the subtitles to download.",
 )
 @click.option(
     "--output-dir",
@@ -39,37 +39,16 @@ def kisskh():
     default=Path.home() / "Downloads",
     help="Output directory where downloaded files will be store.",
 )
-@click.option(
-    "--force-download",
-    "-fd",
-    is_flag=True,
-    default=False,
-    help="Select nearest video quality if expected one not available.",
-)
-@click.option(
-    "--convert-stream-to",
-    "-cs",
-    default="mkv",
-    help="Convert the stream (.ts) to other format (.mkv, .mp4, .avi etc.).",
-)
-@click.option(
-    "--keep-stream-file",
-    "-ks",
-    is_flag=True,
-    default=False,
-    help="Keep the .ts format after the conversion is done.",
-)
 def dl(
     drama_url_or_name: str,
     first: int,
     last: int,
     quality: str,
+    sub_langs: list[str],
     output_dir: Path | str,
-    force_download: bool,
-    convert_stream_to: str,
-    keep_stream_file: bool,
 ) -> None:
     kisskh_api = KissKHApi()
+    downloader = Downloader()
     if validators.url(drama_url_or_name):
         parsed_url = urlparse(drama_url_or_name)
         drama_id = parse_qs(parsed_url.query)["id"][0]
@@ -82,30 +61,15 @@ def dl(
 
     for episode_number, episode_id in episode_ids.items():
         print(f"Getting details for Episode {episode_number}...")
-        url = kisskh_api.get_stream_url(episode_id)
-        if "tickcounter" in url:
+        video_stream_url = kisskh_api.get_stream_url(episode_id)
+        subtitles = kisskh_api.get_subtitles(episode_id, *sub_langs)
+        if "tickcounter" in video_stream_url:
             print(f"Episode {episode_number} still not released!")
             continue
 
-        ms = Mu3u8Downloader(url)
-        videos = ms.get_segments_mapping()
-        downloadable_quality = get_matching_quality(
-            quality, videos.keys(), select_closest_available=force_download
-        )
-        outfile = f"{output_dir}/{drama_name}/{drama_name}_E{episode_number:02d}_{downloadable_quality}.ts"
-        ms.download_playlist_segments(outfile, videos.get(downloadable_quality))
-
-        found_ffmpeg = shutil.which("ffmpeg")
-
-        if found_ffmpeg:
-            print(f"Converting stream to .{convert_stream_to} format...")
-            ffmpeg.input(outfile).output(
-                filename=f"{outfile[:-3]}.{convert_stream_to}",
-                acodec="copy",
-                vcodec="copy",
-            ).global_args("-loglevel", "quiet").run()
-            if not keep_stream_file:
-                os.remove(outfile)
+        filepath = f"{output_dir}/{drama_name}/{drama_name}_E{episode_number:02d}"
+        downloader.download_video_from_stream_url(video_stream_url, filepath, quality)
+        downloader.download_subtitles(subtitles, filepath)
 
 
 if __name__ == "__main__":
--- src/kisskh_downloader/downloader.py
+++ src/kisskh_downloader/downloader.py
@@ -1,105 +1,38 @@
-import queue
+import os
 from pathlib import Path
-from threading import Thread
+from urllib.parse import urlparse
 
-import m3u8
 import requests
-from tqdm import tqdm
+import yt_dlp
+
+from kisskh_downloader.models.sub import SubItem
 
 
-class Worker(Thread):
-    def __init__(self, request_queue, session, tqdm_progress_bar=None):
-        Thread.__init__(self)
-        self.queue = request_queue
-        self.session = session
-        self.responses = []
-        self.progress_bar = tqdm_progress_bar
+class Downloader:
+    def download_video_from_stream_url(self, video_stream_url: str, filepath: str, quality: str) -> None:
+        """Download a video from stream url
 
-    def run(self):
-        while True:
-            content = self.queue.get()
-            if content == "":
-                break
-            index, url = content
-            response = self.session.get(url)
-            self.responses.append((index, response.content))
-            if self.progress_bar:
-                self.progress_bar.update(1)
-            self.queue.task_done()
+        :param video_stream_url: stream url
+        :param filepath: file path where to download
+        :param quality: quality to select
+        """
+        ydl_opts = {
+            "format": f"bestvideo[height<={quality[:-1]}]+bestaudio/best[height<={quality[:-1]}]/best",
+            "concurrent_fragment_downloads": 15,
+            "outtmpl": f"{filepath}.%(ext)s",
+            "postprocessors": [{"key": "FFmpegFixupM3u8"}],
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download(video_stream_url)
 
+    def download_subtitles(self, subtitles: list[SubItem], filepath: str) -> None:
+        """Download subtitles
 
-class Mu3u8Downloader:
-    def __init__(self, url: str):
-        self.url = url
-        self.session = None
-
-    def _get_session(self):
-        if self.session is None:
-            self.session = requests.Session()
-        return self.session
-
-    def get_segments_mapping(self):
-        session = self._get_session()
-        response = session.get(self.url)
-        m3u8_master = m3u8.loads(response.text)
-        if not m3u8_master.data["is_variant"]:
-            # For two kinds of URL:
-            #     https://hls04.daduymeavea.online/hls04/a-business-proposal-2022-episode-1-v1/720.m3u8
-            #     https://hls04.daduymeavea.online/hls04/e21c5e3a133ef02ef786d0c62ba7ca08/ep.14.v0.1622327945.720.m3u8
-            # split in eihter '.' or '/'
-            print("Only 1 video available!")
-            key = "unknown-quality"
-            return {key: [segment["uri"] for segment in m3u8_master.data["segments"]]}
-
-        m3u8_playlists = {}
-
-        for m3u8_playlist in m3u8_master.data["playlists"]:
-            key = m3u8_playlist["stream_info"]["resolution"]
-            response = session.get(m3u8_playlist["uri"])
-            m3u8_master = m3u8.loads(response.text)
-            m3u8_playlists[key] = [
-                segment["uri"] for segment in m3u8_master.data["segments"]
-            ]
-
-        return m3u8_playlists
-
-    def download_playlist_segments(
-        self,
-        outfile: Path | str,
-        playlist_segments: list[str],
-        number_of_workers: int = 16,
-    ):
-        session = self._get_session()
-        segments_dict = dict(zip(range(len(playlist_segments)), playlist_segments))
-        q = queue.Queue()
-        for segment in segments_dict.items():
-            q.put(segment)
-        for _ in range(number_of_workers):
-            q.put("")
-
-        with tqdm(
-            total=len(segments_dict),
-            desc=f"Downloading content for {Path(outfile).name}",
-        ) as progress_bar:
-            workers = []
-            for _ in range(number_of_workers):
-                worker = Worker(q, session, progress_bar)
-                worker.daemon = True
-                worker.start()
-                workers.append(worker)
-
-            for worker in workers:
-                worker.join()
-
-        responses = []
-        for worker in workers:
-            responses.extend(worker.responses)
-
-        Path(outfile).parent.mkdir(parents=True, exist_ok=True)
-        with open(outfile, "wb") as file:
-            responses_dict = dict(responses)
-            for i in tqdm(
-                range(len(responses_dict)),
-                desc=f"Writing content for {Path(outfile).name}...",
-            ):
-                file.write(responses_dict[i])
+        :param subtitles: list of all subtitles
+        :param filepath: file path where to download
+        """
+        for subtitle in subtitles:
+            print(f"Downloading {subtitle.label} sub...")
+            extension = os.path.splitext(urlparse(subtitle.src).path)[-1]
+            response = requests.get(subtitle.src)
+            Path(f"{filepath}.{subtitle.land}{extension}").write_bytes(response.content)
